8ea5793abf715644b4caa7fb7c0cbb760d6caa6a
Dominique Clarke <dominiquedclarke@gmail.com>
2016-11-14 23:47:56 -0600

    added set timeout to iTunes calls

--- server/features/show/showCtrl.js
+++ server/features/show/showCtrl.js
@@ -44,69 +44,73 @@ module.exports = {
 
 function getMusicPreviews(shows) {
      const promiseArray = [];
+     let time = 0;
      shows.Events.forEach(show => {
         show.Artists.forEach(artist => {
             artist.Name = artist.Name.trim();
             const nameQuery = artist.Name.split(" ").join("+");
             promiseArray.push(new Promise((resolve, reject) => {
-            axios.get(`https://itunes.apple.com/search?term=${nameQuery}&entity=musicTrack&limit=10`)
-                .catch(err => {
-                   console.log(err);
-                   resolve({});
-                })
-            .then(response => {
-                const iTunesData = response.data;
-                let artistArtworkUrl;
-                const songPreviews = [];
-                for(let i = 0; i < iTunesData.results.length; i++) {
-                    const song = iTunesData.results[i];
-                    if(song.artistName === artist.Name) {
-                       songPreviews.push({
-                         songName: song.trackName
-                         , artistName: song.artistName
-                         , previewUrl: song.previewUrl
-                         , songArtworkUrl: song.artworkUrl100.replace("100x100", "400x400")
-                    });
-                    //if this is the first result that's come back correct, let's add the artist artwork Url right away
-                    if(songPreviews.length === 1) {
-                        artistArtworkUrl = song.artworkUrl100.replace('100x100', '400x400');
-                    }
-                    if(songPreviews.length === 5) {
-                        break;
-                    }
-                };
-            };
-               /* if no songs were found with the exact artist name, chances are
-               there's a problem with the way JamBase coded it. Let's trust iTunes for
-               now and just assign the artist song previews based on the first
-               five results */
-               if(songPreviews.length === 0 ) {
-                 const shortenedResponse = iTunesData.results.slice(0, 5);
-                 if(shortenedResponse[0]) {
-                   artistArtworkUrl = shortenedResponse[0].artworkUrl100.replace("100x100", "400x400");
-                 }
-                 shortenedResponse.forEach(function(song) {
-                   songPreviews.push({
-                     songName: song.trackName
-                     , artistName: song.artistName
-                     , previewUrl: song.previewUrl
-                     , songArtworkUrl: song.artworkUrl100.replace("100x100", "400x400")
-                   });
-                 });
-               }
+                setTimeout( () => {
+                    time += 200;
+                    axios.get(`https://itunes.apple.com/search?term=${nameQuery}&entity=musicTrack&limit=10`)
+                        .catch(err => {
+                           console.log(err);
+                           resolve({});
+                        })
+                    .then(response => {
+                        const iTunesData = response.data;
+                        let artistArtworkUrl;
+                        const songPreviews = [];
+                        for(let i = 0; i < iTunesData.results.length; i++) {
+                            const song = iTunesData.results[i];
+                            if(song.artistName === artist.Name) {
+                               songPreviews.push({
+                                 songName: song.trackName
+                                 , artistName: song.artistName
+                                 , previewUrl: song.previewUrl
+                                 , songArtworkUrl: song.artworkUrl100.replace("100x100", "400x400")
+                            });
+                            //if this is the first result that's come back correct, let's add the artist artwork Url right away
+                            if(songPreviews.length === 1) {
+                                artistArtworkUrl = song.artworkUrl100.replace('100x100', '400x400');
+                            }
+                            if(songPreviews.length === 5) {
+                                break;
+                            }
+                        };
+                    };
+                       /* if no songs were found with the exact artist name, chances are
+                       there's a problem with the way JamBase coded it. Let's trust iTunes for
+                       now and just assign the artist song previews based on the first
+                       five results */
+                       if(songPreviews.length === 0 ) {
+                         const shortenedResponse = iTunesData.results.slice(0, 5);
+                         if(shortenedResponse[0]) {
+                           artistArtworkUrl = shortenedResponse[0].artworkUrl100.replace("100x100", "400x400");
+                         }
+                         shortenedResponse.forEach(function(song) {
+                           songPreviews.push({
+                             songName: song.trackName
+                             , artistName: song.artistName
+                             , previewUrl: song.previewUrl
+                             , songArtworkUrl: song.artworkUrl100.replace("100x100", "400x400")
+                           });
+                         });
+                       }
 
-             const artistData = {
-               name: artist.Name
-               , artistArtworkUrl
-               , songPreviews
-             }
-             //event.artistData.push(artistData);
-             artist.songPreviews = songPreviews;
-             artist.artistArtworkUrl = artistArtworkUrl;
-             //axios.post('/api/artist', artistData);
-             resolve(show);
-           })
-         }));
+                     const artistData = {
+                       name: artist.Name
+                       , artistArtworkUrl
+                       , songPreviews
+                     }
+                     //event.artistData.push(artistData);
+                     artist.songPreviews = songPreviews;
+                     artist.artistArtworkUrl = artistArtworkUrl;
+                     //axios.post('/api/artist', artistData);
+                     resolve(show);
+                   })
+               }, time);
+            }));
        });
      });
     return Promise.all(promiseArray).then((results) => {
